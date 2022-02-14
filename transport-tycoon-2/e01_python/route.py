2489eef75e418c7ce2d18a6299bea1790c90c66e
Rinat Abdullin <rinat.abdullin@gmail.com>
2022-02-14 15:04:09 +0100

    Add some comments

--- transport-tycoon-2/e01_python/route.py
+++ transport-tycoon-2/e01_python/route.py
@@ -4,6 +4,8 @@ Usage: route.py FROM TO
 
 This implements Trustbit Transport Kata Episode 2.1 - Find shortest path on map
 https://github.com/Softwarepark/exercises/blob/master/transport-tycoon_21.md
+
+The file is formatted with black
 """
 
 import sys
@@ -12,20 +14,20 @@ from collections import defaultdict
 from dataclasses import dataclass
 
 
+# linked list to keep travel history
 @dataclass
 class Leg:
     current: str
     previous: "Leg"
 
 
+# we'll build graph for our map out of these
 @dataclass
 class Road:
     destination: str
     km: int
 
 
-visited = []
-future = PQ()
 graph = defaultdict(list)  # port to roads
 
 with open("s02e01_map.csv") as f:
@@ -36,15 +38,20 @@ with open("s02e01_map.csv") as f:
         graph[b].append(Road(a, int(km)))
 
 _, start, end = sys.argv
-future.put((0, Leg(start, None)))
 
-while not future.empty():
-    (distance, trip) = future.get()
+visited = []
+# priority queue to explore closest locations first
+travels = PQ()
+travels.put((0, Leg(start, None)))
+
+while not travels.empty():
+    (distance, trip) = travels.get()
     location = trip.current
     if location in visited:
         continue
 
     if location == end:
+        # we arrived. Let's reverse the trip history to print it
         path = [trip.current]
         while trip.previous:
             trip = trip.previous
@@ -58,5 +65,6 @@ while not future.empty():
         if road.destination in visited:
             continue
 
+        # this destination wasn't explored, let's check it out
         distance_at = distance + road.km
-        future.put((distance_at, Leg(road.destination, trip)))
+        travels.put((distance_at, Leg(road.destination, trip)))
