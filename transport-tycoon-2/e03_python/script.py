3c5ba3585284bddf8bfe849b990e6b3996cef948
Rinat Abdullin <rinat.abdullin@gmail.com>
2022-03-28 14:40:17 +0200

    Cleanup the code

--- transport-tycoon-2/e03_python/script.py
+++ transport-tycoon-2/e03_python/script.py
@@ -4,10 +4,9 @@ from datetime import datetime
 import numpy as np    
 
 
-HISTORY = defaultdict(list)  # port to roads
+HISTORY = defaultdict(list)  
 
-# Let's parse and load the map
-# but that wouldn't be much fun
+# Let's parse and load the training data
 with open("s02e03_train.csv") as f:
     lines = f.readlines()
     
@@ -19,35 +18,17 @@ with open("s02e03_train.csv") as f:
         time = datetime.strptime(time, '%Y-%m-%d %H:%M:%S')
       
         HISTORY[(a,b)].append((time,speed))
-        
-#HISTORY 
 
-
-# this is our map
-# key is the current location
-# value is a list of (location, Time-to-travel)
-MAP = {}  # port to roads
-
-
+# this is our map with computed data
+MAP = {}
 
 for (a,b), history in HISTORY.items():
-    
-   
     speeds = [x[1] for x in history]
     times = [x[0].time().hour for x in history]
 
+    model = np.poly1d(np.polyfit(times, speeds, 3))
 
-    average_speed = sum(speeds)/len(history)
-    
-    coefs = np.poly1d(np.polyfit(times, speeds, 3))
-
-    MAP[(a,b)] = (average_speed, coefs)
-    
-    
-
-
-
-
+    MAP[(a,b)] = model
 
 
 errors = 0
@@ -62,9 +43,8 @@ with open("s02e03_test.csv") as f:
         # 1859-11-26 19:43:31
         time = datetime.strptime(time, '%Y-%m-%d %H:%M:%S')
     
-        
-        predicted, coef = MAP[(a, b)]
-        predicted = coef(time.time().hour)
+        model = MAP[(a, b)]
+        predicted = model(time.time().hour)
         
         
         errors += (predicted - observed) * (predicted - observed)
